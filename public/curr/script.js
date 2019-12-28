fd0c010fe03f2f28b0505b8f953c840d941c551d
Dmitry Parzhitsky <parzhitsky@gmail.com>
2019-12-28 21:42:06 +0200

    Add "/curr" path

---
+++ public/curr/script.js
@@ -0,0 +1,119 @@
+// @ts-check
+/// <reference lib="dom" />
+/// <reference lib="es2017" />
+/// <reference path="./types.d.ts" />
+
+window.Curr = window.Curr || {
+	EXPRESSION: "(50 USD + 15 EUR) / 3 + 10 EUR - 500 UAH",
+	URL_BASE: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&valcode=",
+
+	rates: Object.create(null),
+
+	convert(code, amount) {
+		return amount * (window.Curr.rates[code] || 1);
+	},
+
+	toUAH(code, amount) {
+		return `${ window.Curr.convert(code, amount).toFixed(2) } UAH`;
+	},
+
+	async fetchRate(code) {
+		const { rates } = window.Curr;
+
+		if (code in rates === false) {
+			let rate = 1;
+
+			try {
+				const response = await fetch(window.Curr.URL_BASE + code);
+
+				/** @type {RatesDataRaw} */
+				const json = await response.json();
+
+				rate = json[0].rate;
+			}
+
+			catch (error) {
+				console.warn(`Could not fetch actual rate of the currency "${ code }"; fallback to 1.00`);
+			}
+
+			rates[code] = rate;
+		}
+
+		return rates[code];
+	},
+};
+
+window.CurrDOM = window.CurrDOM || {
+	ratesContainer: window.document.getElementById("rates-container"),
+	expressionsContainer: window.document.getElementById("expressions-container"),
+	expressionNode: window.document.getElementById("expression"),
+	expressionConvertedNode: window.document.getElementById("expression-converted"),
+	resultContainer: window.document.getElementById("result-container"),
+	resultNode: window.document.getElementById("result"),
+
+	clearNode(node) {
+		while (node.firstChild)
+			node.removeChild(node.firstChild);
+	},
+
+	setNodeText(node, text) {
+		window.CurrDOM.clearNode(node);
+		node.insertAdjacentText("afterbegin", text);
+	},
+
+	createRateNode(code) {
+		let node = window.document.querySelector(`[data-code=${ code }]`);
+
+		if (!node) {
+			node = window.document.createElement("p");
+	
+			node.classList.add("rate");
+			node.setAttribute("data-code", code);
+		}
+
+		else window.CurrDOM.clearNode(node);
+
+		return {
+			rateNode: node,
+			exists: !!node,
+		};
+	},
+
+	async showRate(code) {
+		const rate = window.Curr.fetchRate(code);
+		const { rateNode, exists } = window.CurrDOM.createRateNode(code);
+
+		rateNode.insertAdjacentText("afterbegin", String(await rate));
+
+		if (!exists)
+			window.CurrDOM.ratesContainer.append(rateNode);
+	},
+};
+
+(async () => {
+	const {
+		Curr: {
+			EXPRESSION,
+			toUAH,
+		},
+		CurrDOM: {
+			expressionNode,
+			expressionConvertedNode,
+			resultNode,
+			setNodeText,
+			showRate,
+		},
+	} = window;
+
+	/** @private */
+	const currencyCodes = [ "USD", "EUR" ];
+
+	await Promise.all(currencyCodes.map(showRate));
+
+	const expressionConverted = `\t(${ toUAH("USD", 50) } + ${ toUAH("EUR", 15) }) / 3 + ${ toUAH("EUR", 10) } - 500 UAH`;
+	const result = eval(expressionConverted.replace(/ UAH/g, ""));
+
+	setNodeText(expressionNode, EXPRESSION);
+	setNodeText(expressionConvertedNode, expressionConverted);
+	setNodeText(resultNode, toUAH("", result));
+})();
---
+++ public/curr/types.d.ts
@@ -0,0 +1,41 @@
+interface Window {
+	Curr: {
+		EXPRESSION: string;
+		URL_BASE: string;
+		rates: Record<string, number>;
+
+		convert(code: string, amount: number): number;
+		toUAH(code: string, amount: number): string;
+		fetchRate(code: string): Promise<number>;
+	};
+
+	CurrDOM: {
+		ratesContainer: HTMLElement;
+		expressionsContainer: HTMLElement;
+		expressionNode: HTMLElement;
+		expressionConvertedNode: HTMLElement;
+		resultContainer: HTMLElement;
+		resultNode: HTMLElement;
+
+		clearNode(node: Element): void;
+		setNodeText(node: Element, text: string): void;
+		createRateNode(code: string): {
+			rateNode: Element;
+			exists: boolean;
+		};
+		showRate(code: string): Promise<void>;
+	};
+}
+
+interface RatesDataRawItem {
+	// essential
+	rate: number;
+	cc: string;
+
+	// junk
+	r030: number;
+	txt: string;
+	exchangedate: string;
+}
+
+type RatesDataRaw = RatesDataRawItem[];
