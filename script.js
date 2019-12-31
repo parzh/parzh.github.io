387389b72e21a5a3196109e9f72a14c2c09d2cc1
Dmitry Parzhitsky <parzhitsky@gmail.com>
2019-12-31 14:31:40 +0200

    Fix possible NPE Weird, but "tsc --project" did work

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("content").insertAdjacentText("beforeend", ", move along!");
+{
+	const content = document.getElementById("content");
+
+	if (!content)
+		throw new Error("Element '#content' is not found!");
+
+	else
+		content.insertAdjacentText("beforeend", ", move along!");
+}
