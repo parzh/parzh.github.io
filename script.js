6abe6a5971d66a7078bc3d140fbe0e56f242d635
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-01 17:22:09 +0200

    Build from 42a83edb1160c1f60aedb8b168857cf80dd1fb17

--- script.js
+++
@@ -1,9 +0,0 @@
-{
-	const content = document.getElementById("content");
-
-	if (!content)
-		throw new Error("Element '#content' is not found!");
-
-	else
-		content.insertAdjacentText("beforeend", ", move along!");
-}
