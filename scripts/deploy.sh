e31ad9172502d9c5354b8202661a5aea28971513
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-02 20:33:37 +0200

    Revert "Try setting up git inside Travis"
    This reverts commit 581a64a0a034483309ffff5d4bc618ac8f7724b6.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# configure git
-git config user.name "${GH_USER}" && \
-git config user.email "${GH_EMAIL}" && \
-git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/* && \
-
 # checkout `master` branch in `_site` folder
-git fetch --unshallow origin master && \
-git worktree add -b _site master origin/master && \
+git fetch --all && \
+git worktree add _site master && \
 
 # populate `_site`
 bundle exec jekyll build && \
