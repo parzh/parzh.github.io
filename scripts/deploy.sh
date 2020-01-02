25d59c7707177ce5312c8148d402058cd8736322
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-02 20:37:08 +0200

    Make the script copy-pasteable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,6 @@ bundle exec jekyll build && \
 # update `master`
 cd _site && \
 git add . && \
-git commit --allow-empty -m "Build from $(git log --format=format:%H develop -1)" && \
+git commit --allow-empty -m "$(echo Build from $(git log --format=format:%H develop -1))" && \
 git push origin master --no-verify && \
 cd ..
