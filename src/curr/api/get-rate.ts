294b4e62bad04f9b7114c42a0c55098490865fc0
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-08 20:08:59 +0200

    Use more obvious flow of logs

--- src/curr/api/get-rate.ts
+++ src/curr/api/get-rate.ts
@@ -9,22 +9,22 @@ async function _getRate(code: string, attempts: number): Promise<number> {
 	if (code in rates === false) {
 		let rate = 1;
 
+		if (attempts > 0)
+			console.log(`Fetching "${ code }" (${ attempts } / ${ MAX_ATTEMPTS })`);
+
 		try {
 			attempts++;
 			rate = await fetchRate(code);
 		}
 
 		catch (error) {
-			console.warn(`Could not fetch actual rate of the currency "${ code }"`);
+			console.warn(`Could not fetch actual rate of "${ code }"`);
 			console.error(error);
 
-			if (attempts < MAX_ATTEMPTS) {
-				console.log(`Fetching again (${ attempts + 1 /* showing near future */ } / ${ MAX_ATTEMPTS })`);
-
+			if (attempts < MAX_ATTEMPTS)
 				return await _getRate(code, attempts);
-			}
 
-			console.warn(`Reached fetch attempts limit; fallback to 1.00`);
+			console.warn(`Reached fetch attempts limit; fallback "${ code }" to 1.00`);
 		}
 
 		rates[code] = rate;
