9e381e1a2fd408e197553715ad8b781b0811a709
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-11 00:15:15 +0200

    Fix eslint errors

--- test/helpers/assert.ts
+++ test/helpers/assert.ts
@@ -1,4 +1,4 @@
-export default function assert(value: any, message: string): void {
+export default function assert(value: unknown, message: string): void {
 	if (!value) {
 		process.nextTick(process.exit, 1);
 		throw new Error(`AssertionError: ${ message }`);
