21e036bfd82b3cab2946fe45421395a4abacae55
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-11 18:20:47 +0200

    Remove everything test-related

--- test/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-	interface ProcessEnv {
-		PACKAGE_HOME: string;
-	}
-}
--- test/helpers/assert.ts
+++
@@ -1,6 +0,0 @@
-export default function assert(value: unknown, message: string): void {
-	if (!value) {
-		process.nextTick(process.exit, 1);
-		throw new Error(`AssertionError: ${ message }`);
-	}
-}
