22b09037f8ed283288f13e025fdc949f29b605df
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-11 13:10:37 +0200

    Mark internal entity

--- src/curr/api/fetch-rate.ts
+++ src/curr/api/fetch-rate.ts
@@ -16,6 +16,7 @@ type RatesDataRaw = RatesDataRawItem[];
 /** @private */
 const URL_BASE = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&valcode=";
 
+/** @internal */
 export default async function fetchRate(code: string): Promise<number> {
 	const response = await fetch(URL_BASE + code);
 	const [ data ] = await response.json() as RatesDataRaw;
