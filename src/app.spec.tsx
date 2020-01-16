ce1d396d56dbdc4f639e1b32eeb3e3f36f931bf7
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-16 23:21:13 +0200

    Install Enzyme

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 
 import App from "./app";
 
 describe("<App /> (index)", () => {
 	it("holds the correct text", () => {
-		expect(render(<App />).container).toHaveTextContent("Nothing's here, move along!");
+		expect(shallow(<App />).text()).toEqual<string>("Nothing's here, move along!");
 	});
 });
