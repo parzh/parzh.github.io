99798bbd6824fc7517d0a4ef0fe57d6c49a89e06
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-01-19 18:57:15 +0200

    Simplify the code

--- src/curr/components/input-container.tsx
+++ src/curr/components/input-container.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./input-container.css";
 
 /** @private */
-type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
-
-/** @private */
 interface OnChange {
 	(input: string | null): unknown;
 }
@@ -23,16 +20,12 @@ const INITIAL_VALUE = "(50 USD + 15 EUR) / 3 + 10 EUR - 500 UAH";
 /** @private */
 const expressionRegex = /^[\d,.A-Z ()*/+-]+$/;
 
-/** @private */
-function handleChange(event: ChangeEvent, onChange: OnChange): unknown {
-	const { value } = event.currentTarget;
-	const verified = expressionRegex.test(value);
-
-	return onChange(verified ? value : null);
-}
-
 export default function InputContainer({ onChange = noop }: Props): JSX.Element {
-	const eventHandler = (event: ChangeEvent): unknown => handleChange(event, onChange);
+	const eventHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const { value } = event.currentTarget;
+
+		onChange(expressionRegex.test(value) ? value : null);
+	};
 
 	return (
 		<section className="InputContainer">
