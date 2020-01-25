59e5208a2279c4988f39ee70ecc1bbf9e2f573a4
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-25 22:27:24 +0200

    Create working example
    It is based on HashRouter due to limitations of using GitHub Pages

--- src/spa/app.tsx
+++ src/spa/app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
 import UserPage from "./user-page";
 import HomePage from "./home-page";
@@ -6,8 +7,23 @@ import HomePage from "./home-page";
 export default function App(): JSX.Element {
 	return (
 		<>
-			<UserPage />
-			<HomePage />
+			<section className="links">
+				<p>
+					<Link to="/home">Go to /home</Link>
+				</p>
+				<p>
+					<Link to="/user">Go to /user</Link>
+				</p>
+			</section>
+
+			<Switch>
+				<Route path="/home">
+					<HomePage />
+				</Route>
+				<Route path="/user">
+					<UserPage />
+				</Route>
+			</Switch>
 		</>
 	);
 }
--- src/spa/index.tsx
+++ src/spa/index.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/** @private */
+const app = (
+	<HashRouter>
+		<App />
+	</HashRouter>
+);
+
+/** @private */
+const root = document.getElementById("root");
+
+ReactDOM.render(app, root);
