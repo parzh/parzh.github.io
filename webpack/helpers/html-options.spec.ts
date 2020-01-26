4dbb59f15d82ca9fe0d0a6c201cdce042c7cb55e
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-26 16:16:08 +0200

    Cover webpack helpers with unit tests

---
+++ webpack/helpers/html-options.spec.ts
@@ -0,0 +1,46 @@
+import { resolve } from "path";
+import htmlOptions from "./html-options";
+
+/** @private */
+const pathEqualityMapEntries = Object.entries({
+	[resolve(__dirname, "../dist/index.html")]: [
+		"/",
+		"index.html",
+		"/index.html",
+	],
+	[resolve(__dirname, "../dist/smth.html")]: [
+		"smth.html",
+		"/smth.html",
+	],
+	[resolve(__dirname, "../dist/smth/index.html")]: [
+		"smth/",
+		"/smth/",
+		"smth/index.html",
+		"/smth/index.html",
+	],
+});
+
+/** @private */
+function getResolvedTemplatePath(path: string): string | undefined {
+	return htmlOptions("", path, []).filename;
+}
+
+describe("htmlOptions (webpack helper)", () => {
+	const options = htmlOptions("Something", "some.html", []);
+
+	it("produces options for HtmlWebpackPlugin constructor", () => {
+		expect(options.constructor).toBe(Object);
+	});
+
+	it("automatically adds instruction to remove comments", () => {
+		expect(options).toHaveProperty("minify.removeComments", true);
+	});
+
+	it("resolves target template path relative to dist/ dir", () => {
+		for (const [ target, pathVariants ] of pathEqualityMapEntries)
+			for (const path of pathVariants)
+				expect(getResolvedTemplatePath(path)).toEqual(target);
+
+		expect(() => getResolvedTemplatePath("smth")).toThrow();
+	});
+});
