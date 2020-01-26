16632a73766313452a715913c59acc237e1bdf1e
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-26 17:13:02 +0200

    Add cross-links

--- src/spa/home-page.tsx
+++ src/spa/home-page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage(): JSX.Element {
-	return <div>Home page</div>;
+	return <>
+		<div>Home page</div>
+
+		<p>
+			<Link to="/user">Go to /user</Link>
+		</p>
+	</>;
 }
--- src/spa/user-page.tsx
+++ src/spa/user-page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function UserPage(): JSX.Element {
-	return <div>User page</div>;
+	return <>
+		<div>User page</div>
+
+		<p>
+			<Link to="/home">Go to /home</Link>
+		</p>
+	</>;
 }
