4ae521c9e360ad98509fa1ad70819d0636341141
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-24 18:32:30 +0300

    Remove redundant type assertion

--- webpack/webpack.config.dev.ts
+++ webpack/webpack.config.dev.ts
@@ -1,9 +1,8 @@
-import { Configuration } from "webpack";
 import { smart as extend } from "webpack-merge";
 import prod from "./webpack.config.prod";
 
 /** @public */
-const config: Configuration = extend(prod, {
+const config = extend(prod, {
 	mode: "development",
 	devtool: "source-map",
 	devServer: {
