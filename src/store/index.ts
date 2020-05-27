34573dbcdb1bd17d26fd91167245f63177bbb35f
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-27 16:49:38 +0300

    Define type of the root state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,12 @@
+import type { CombinedState } from "redux";
 import { createStore, combineReducers } from "redux";
+
+import type UserState from "./user";
 import user from "./user/reducer";
+
+export type State = CombinedState<{
+	user: UserState;
+}>;
 
 export default createStore(combineReducers({
 	user,
