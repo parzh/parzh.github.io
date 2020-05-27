817c640acbfc39dc3ad68d4938ca78de1e64bdbc
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-27 16:51:15 +0300

    Define <ProfileTitleReadonly>

---
+++ src/components/profile/profile-title-readonly.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Title from "src/components/title";
+import { useName } from "src/store/selectors";
+import { createStyleFor } from "src/helpers/styles";
+
+/** @private */
+const styleFor = createStyleFor({
+	editText: {
+		cursor: "pointer",
+	},
+	editIcon: {
+		verticalAlign: "middle",
+	},
+});
+
+/** @private */
+interface Props {
+	onRename?: () => void;
+}
+
+export default function ProfileTitleReadonly({ onRename }: Props): JSX.Element {
+	const name = useName();
+
+	return (
+		<>
+			<Title>{name}</Title>
+
+			<span className="text-muted text-center" onClick={onRename}>
+				<i
+					className="material-icons small mr-1"
+					style={styleFor.editIcon}
+				>
+					create
+				</i>
+				<u className="small" style={styleFor.editText}>
+					rename
+				</u>
+			</span>
+		</>
+	);
+}
